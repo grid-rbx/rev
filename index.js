25ea1dc6ff84e5d4e1b5c4705cca5a420fc194e9
curse <58047023+cursecodes@users.noreply.github.com>
2021-11-19 08:46:19 -0700

    Discord part almost ready.

--- index.js
+++ index.js
@@ -1,37 +1,101 @@
-import fetch from "node-fetch"
+const fetch = require("node-fetch");
 
-function check(id) {
+const config = {
+  userNotFound: {
+    status: "error",
+    errorCode: 404,
+    error: "User not found.",
+  },
+  noDiscordId: {
+    status: "error",
+    errorCode: 400,
+    error: "No Discord ID provided.",
+  },
+};
+
+function checkDiscordId(id) {
   if (!id) {
-    return {
-      "error": "No Discord ID provided."
-    }
+    return console.log(config.noDiscordId);
   }
-  
-  return fromRover(id)
+
+  id = id.toString();
+
+  const bloxlink = fromBloxlink(id);
+  const rover = fromRover(id);
+
+  if (bloxlink.status === "http-error" || rover.status === "http-error") {
+    return console.log(bloxlink.error);
+  }
+
+  if (bloxlink.status === "error" && rover.status === "error") {
+    return console.log({
+      bloxlink,
+      rover,
+    });
+  } else if (bloxlink.status === "error") {
+    return console.log(bloxlink);
+  } else if (rover.status === "error") {
+    return console.log(rover);
+  } else if (bloxlink.status === "ok" && rover.status === "ok") {
+    return bloxlink;
+  } else if (bloxlink.status === "ok" && rover.status === "error") {
+    return bloxlink;
+  } else if (bloxlink.status === "error" && rover.status === "ok") {
+    return rover;
+  }
+
+  return id;
 }
 
-function fromRover(id) {
+async function fromRover(id) {
   try {
-    const response = fetch(`https://verify.eryn.io/api/user/${id}`).json()
-    
-    return {
-      "id": id,
-      "robloxId": response.robloxId,
-      "username": response.username
+    const response = await fetch(`https://verify.eryn.io/api/user/${id}`);
+    const body = await response.json();
+
+    if (body.statusCode === "error") {
+      return config.userNotFound;
     }
+    return {
+      status: "ok",
+      statusCode: 200,
+      discordId: id,
+      robloxId: body.robloxId,
+    };
   } catch (error) {
-    return error
+    return {
+      status: "http-error",
+      error,
+    };
   }
 }
 
-const user = check("862719183447130122")
+async function fromBloxlink(id) {
+  try {
+    const response = await fetch(`https://api.blox.link/v1/user/${id}`);
+    const body = await response.json();
 
-setTimeout(() => {
-  console.log(user)
-}, 10000)
+    if (body.status === "error") {
+      return config.userNotFound;
+    }
+
+    return {
+      status: "ok",
+      statusCode: 200,
+      discordId: id,
+      robloxId: body.primaryAccount,
+    };
+  } catch (error) {
+    return {
+      status: "http-error",
+      error,
+    };
+  }
+}
+
+function checkForCode(id, code) {}
 
 const rev = {
-  check
-}
+  checkDiscordId,
+};
 
-export default check
+module.exports = rev;
---
+++ test.js
@@ -0,0 +1,3 @@
+const rev = require("./index");
+
+rev.checkDiscordId(1234);
