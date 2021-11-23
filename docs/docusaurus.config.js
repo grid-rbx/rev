28c33d1f0a08cf858c83e21694e148cb69429d84
curse <58047023+cursecodes@users.noreply.github.com>
2021-11-22 18:15:53 -0700

    fixed broken links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,20 +49,8 @@ const config = {
     // },
     colorMode: {
       defaultMode: "dark",
-      disableSwitch: false,
+      disableSwitch: true,
       respectPrefersColorScheme: false,
-      switchConfig: {
-        darkIcon: "🌙",
-        darkIconStyle: {
-          marginLeft: "2px",
-        },
-        // Unicode icons such as '\u2600' will work
-        // Unicode with 5 chars require brackets: '\u{1F602}'
-        lightIcon: "🌞",
-        lightIconStyle: {
-          marginLeft: "1px",
-        },
-      },
     },
     navbar: {
       title: "Rev",
@@ -101,7 +89,7 @@ const config = {
             },
             {
               label: "API Reference",
-              to: "/docs/api/",
+              to: "/docs/",
             },
           ],
         },
