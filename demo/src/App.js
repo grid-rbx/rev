dee607db07234d987043557e04821d4250a6fc48
curse <58047023+cursecodes@users.noreply.github.com>
2021-11-27 14:12:42 -0700

    (multi) Changes, fixes

--- demo/src/App.js
+++ demo/src/App.js
@@ -9,11 +9,10 @@ function App() {
       <header className="App-header">
         <h1>Rev Demo</h1>
         <p>
-          This is a demo for the Rev library, built with React. You can take the
-          words <br /> below, put them in your Roblox description, then insert
-          your Roblox ID <br /> into the textbox below then press on the button
-          labeled 'Check' and it will tell you if <br /> the code was found on
-          your profile!
+          This is a demo for the Rev library. You can take the words <br />{" "}
+          below, put them in your Roblox description, then insert your Roblox ID{" "}
+          <br /> into the textbox below then press on the button labeled 'Check'
+          and it will tell you if <br /> the code was found on your profile!
         </p>
         <p>
           <i>Reload the page to get a new code.</i>
